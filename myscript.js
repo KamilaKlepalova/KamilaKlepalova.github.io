ca7fc16371f2cd4af3a225e4bf2e957f462d6435
KamilaKlepalova <62485947+KamilaKlepalova@users.noreply.github.com>
2022-03-27 13:37:52 +0200

    Update myscript.js
    added line graph

--- myscript.js
+++ myscript.js
@@ -597,3 +597,264 @@ function barChartRace() {
     }
 }
 barChartRace();
+
+function vaccLineChart() {
+    const width = 960;
+    const height = 500;
+    const margin = 5;
+    const padding = 25;
+    const adj = 40;
+    const svg = d3.select("#lineChartvaccPercentage").append("svg")
+        .attr("preserveAspectRatio", "xMinYMin meet")
+        .attr("viewBox", "-"
+            + adj + " -"
+            + adj + " "
+            + (width + adj * 3) + " "
+            + (height + adj * 3))
+        .style("padding", padding)
+        .style("margin", margin)
+        .classed("svg-content", true);
+
+    //-----------------------------DATA------------------------------//
+    const timeConv = d3.timeParse("%d-%b-%Y");
+    //const dataset = d3.csv("data/more_data.csv");
+    const dataset = d3.csv("covid19data-vacc.csv");
+    dataset.then(function (data) {
+        //data.columns.slice(1) returns csv headers without first column header (index 0)
+        const slices = data.columns.slice(1).map(function (id) {
+            return {
+                id: id,
+                values: data.map(function (d) {
+                    return {
+                        date: timeConv(d.date),
+                        measurement: +d[id]
+                    };
+                })
+            };
+        });
+
+        console.log("Column headers", data.columns);
+        console.log("Column headers without date", data.columns.slice(1));
+        // returns the sliced dataset
+        console.log("Slices", slices);
+        // returns the first slice
+        console.log("First slice", slices[0]);
+        // returns the array in the first slice
+        console.log("An array", slices[0].values);
+        // returns the date of the first row in the first slice
+        console.log("Date element", slices[0].values[0].date);
+        // returns the array's length
+        console.log("Array length", (slices[0].values).length);
+
+
+        //----------------------------SCALES-----------------------------//
+
+        const xScale = d3.scaleTime().range([0, width]);
+        const yScale = d3.scaleLinear().rangeRound([height, 0]);
+        xScale.domain(d3.extent(data, function (d) {
+            return timeConv(d.date)
+        }));
+        yScale.domain([(0), d3.max(slices, function (c) {
+            return d3.max(c.values, function (d) {
+                return d.measurement + 4;
+            });
+        })
+        ]);
+
+
+        //-----------------------------AXES------------------------------//
+
+        // const yaxis = d3.axisLeft().scale(yScale); 
+        // const xaxis = d3.axisBottom().scale(xScale);
+
+        const yaxis = d3.axisLeft()
+            // .ticks((slices[0].values).length)
+            .scale(yScale);
+
+        const xaxis = d3.axisBottom()
+            // .ticks(d3.timeDay.every(1))
+            .tickFormat(d3.timeFormat('%b %d'))
+            .scale(xScale);
+
+        //----------------------------LINES------------------------------//
+
+        const line = d3.line()
+            .x(function (d) { return xScale(d.date); })
+            .y(function (d) { return yScale(d.measurement); });
+
+        // add IDs to lines to differentiate between them
+        let id = 0;
+        const ids = function () {
+            return "line-" + id++;
+        }
+
+        //---------------------------TOOLTIP----------------------------//
+
+        const tooltip = d3.select("body").append("div")
+            .attr("class", "tooltip")
+            .style("opacity", 0)
+            .style("position", "absolute");
+
+        //-------------------------2. DRAWING----------------------------//
+
+        //-----------------------------AXES------------------------------//
+
+        svg.append("g")
+            .attr("class", "axis")
+            .attr("transform", `translate(0, ${height})`)
+            .call(xaxis);
+
+        svg.append("g")
+            .attr("class", "axis")
+            .call(yaxis)
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("dy", ".75em")
+            .attr("y", 6)
+            .style("text-anchor", "end")
+            .text("% of population vaccinated");
+
+        //----------------------------LINES------------------------------//
+
+        // the color part
+        const color = d3.scaleOrdinal()
+            .range(['#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00', '#ffff33', '#a65628', '#f781bf', '#999999']);
+
+        const lines = svg.selectAll("lines")
+            .data(slices)
+            .enter()
+            .append("g")
+
+        lines.append("path")
+            .attr("class", "line")
+            // the color part
+            .attr("stroke", function (d) { return color(d[0]) })
+            .attr("d", function (d) { return line(d.values) });
+
+        lines.append("text")
+            .attr("class", "serie_label")
+            .datum(function (d) {
+                return {
+                    id: d.id,
+                    value: d.values[d.values.length - 1]
+                };
+            })
+            .attr("transform", function (d) {
+                return "translate(" + (xScale(d.value.date) + 10)
+                    + "," + (yScale(d.value.measurement) + 5) + ")";
+            })
+            .attr("x", 5)
+            // print line text
+            .text(function (d) { return d.id; });
+
+        const ghost_lines = lines.append("path")
+            .attr("class", "ghost-line")
+            .attr("d", function (d) { return line(d.values); });
+
+
+        //---------------------------POINTS-----------------------------// 
+
+        lines.selectAll("points")
+            .data(function (d) { return d.values })
+            .enter()
+            .append("circle")
+            .attr("cx", function (d) { return xScale(d.date); })
+            .attr("cy", function (d) { return yScale(d.measurement); })
+            .attr("r", 1)
+            .attr("class", "point")
+            .style("opacity", 1);
+
+        //---------------------------EVENTS-----------------------------// 
+
+        // create invisible circles around each point to increase the area to be hovered
+        lines.selectAll("circles")
+            .data(function (d) { return (d.values); })
+            .enter()
+            .append("circle")
+            .attr("cx", function (d) { return xScale(d.date); })
+            .attr("cy", function (d) { return yScale(d.measurement); })
+            .attr('r', 10)
+            .style("opacity", 0)
+            .on('mouseover', function (d) {
+                const pageX = d3.event.pageX;
+                const pageY = d3.event.pageY;
+                tooltip.transition()
+                    .delay(30)
+                    .duration(200)
+                    .style("opacity", 1);
+
+                tooltip.html(d.measurement)
+                    .style("left", (pageX + 25) + "px")
+                    .style("top", (pageY) + "px");
+
+                const selection = d3.select(this).raise();
+
+                selection
+                    .transition()
+                    .delay("20")
+                    .duration("200")
+                    .attr("r", 6)
+                    .style("opacity", 1)
+                    .style("fill", "#ed3700");
+            })
+
+            .on("mouseout", function (d) {
+                tooltip.transition()
+                    .duration(100)
+                    .style("opacity", 0);
+
+                const selection = d3.select(this);
+
+                selection
+                    .transition()
+                    .delay("20")
+                    .duration("200")
+                    .attr("r", 10)
+                    .style("opacity", 0);
+            });
+
+        svg.selectAll(".ghost-line")
+            .on('mouseover', function () {
+                const selection = d3.select(this).raise();
+
+                selection
+                    .transition()
+                    .delay("100").duration("10")
+                    .style("stroke", "#ed3700")
+                    .style("opacity", "1")
+                    .style("stroke-width", "3");
+
+                // add the legend action
+                const legend = d3.select(this.parentNode)
+                    .select(".serie_label");
+
+                legend
+                    .transition()
+                    .delay("100")
+                    .duration("10")
+                    .style("fill", "#2b2929");
+            })
+
+            .on('mouseout', function () {
+                const selection = d3.select(this)
+                selection
+                    .transition()
+                    .delay("100")
+                    .duration("10")
+                    .style("stroke", "#d2d2d2")
+                    .style("opacity", "0")
+                    .style("stroke-width", "10");
+
+                // add the legend action
+                const legend = d3.select(this.parentNode)
+                    .select(".serie_label");
+                legend
+                    .transition()
+                    .delay("100")
+                    .duration("10")
+                    .style("fill", "#d2d2d2");
+            });
+    });
+}
+
+vaccLineChart();
